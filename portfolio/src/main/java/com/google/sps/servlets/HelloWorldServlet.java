32e9b8850e8f8e5c6bb9357439f35f8acd733ea2
Zhe He <jackhe313@g.ucla.edu>
2022-06-10 19:58:49 +0000

    resolve comment, ordering the import by alphbetic order

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,8 +1,8 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
-import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
