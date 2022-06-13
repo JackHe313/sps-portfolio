e1ac9a59b2d601ff80341a12075ea692c67fb3d5
Zhe He <jackhe313@g.ucla.edu>
2022-06-13 20:56:08 +0000

    implement database

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,5 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +23,17 @@ public class FormHandlerServlet extends HttpServlet {
 
     // Print the value so you can see it in the server logs.
     System.out.println(textValueName + " submitted contact email: " + textValueEmail);
+
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactor = datastore.newKeyFactory().setKind("contact");
+    FullEntity contactEntity = 
+        Entity.newBuilder(keyFactor.newKey())
+            .set("name", textValueName)
+            .set("email", textValueEmail)
+            .build();
+    
+    datastore.put(contactEntity);
+
 
     // Write the value to the response so the user can see it.
     response.getWriter().println(textValueName + " submitted contact email: " + textValueEmail);
